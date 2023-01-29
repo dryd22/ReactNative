9f889a9ac16a3c2cd8a81290d88d11bc510484c7
dryd22 <104089664+dryd22@users.noreply.github.com>
2023-01-29 10:27:44 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,34 @@
 /**
 Button test code with returned messages to the screen user for a survey test.
 1/28/2023
-Andrew Gull using react native in expo environment.
+Andrew Gull using react native in expo environment. i OS aprpeas to color font rather than button.
 **/
 import React, { Component } from 'react';  
 import { ImageBackground, Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native';  
  
-const image = { uri: "https://reactjs.org/logo-og.png" };  
-//attempt to add background image
- const App = () => (
-  <View style={styles2.container}>
-    <ImageBackground source={image} resizeMode="cover" style={styles2.image}>
-      <Text style={styles2.text}>Survey</Text>
-    </ImageBackground>
-  </View>
-);
+const image = { uri: "https://pics.me.me/thumb_take-our-survey-right-meow-memecrunch-com-cat-survey-50110191.png" };  
+
+
  
 export default class ButtonBasics extends Component {   
     render() {  
         return (  
-            <View style={styles.container}>  
+            <View style={styles.container}> 
+<ImageBackground source={image} resizeMode="contain" style={styles.image}>
+      <Text style={styles.text}>It's Survey Time</Text>
+    </ImageBackground>			
 	 
                 <View style={styles.buttonGroup}>  
                     <Button  
                         onPress={this.onPressButton}
 	    
-                        title="Happy" 
+                        title=" Happy " 
                         color="#33cc33"
 			onPress={() => Alert.alert('FANTASTIC!')}   
                     />  
                     <Button  
                         onPress={this.onPressButton}  
-                        title="Ok"  
+                        title="  Ok  "  
                         color="#9ae59a"
 			onPress={() => Alert.alert('Nice')}     
                     />  
@@ -47,8 +44,8 @@ export default class ButtonBasics extends Component {
                         onPress={this.onPressButton}  
                         title="Terrible!" 
 						color="#ff0000"						
-			onPress={() => Alert.alert('Forgive Us')}     
-                    />  
+			onPress={() => Alert.alert('Forgive Us')}
+					/>  
                    
                 </View>  
             </View>  
@@ -59,43 +56,31 @@ export default class ButtonBasics extends Component {
 const styles = StyleSheet.create({  
     container: {  
         flex: 2,
-	    //adds white to top and bottom margins.
 		marginBottom: 20,
 		marginTop: 50,
         backgroundColor:"#000033",  
-        justifyContent: 'center', 
-		
-    },  
-
-    buttonGroup: {  
-        margin:100,
-		width: 150,
-		height: 150,
-		
-		
-    },  
-    buttonGroup: {  
-        margin: 50,
-		//flex:1,		
-        flexDirection: 'row', 
-		flexWrap: 'wrap',		
-        justifyContent: 'space-between'  
-    }  
-})
-  const styles2 = StyleSheet.create({
-  container: {
+        justifyContent: 'center',	
+    },
+	image: {
     flex: 1,
-  },
-  image: {
-    flex: 3,
     justifyContent: "center"
   },
   text: {
+	  
     color: "white",
-    fontSize: 42,
-    lineHeight: 84,
+    fontSize: 50,
+    lineHeight: 52,
     fontWeight: "bold",
     textAlign: "center",
-    backgroundColor: "#000000c0"
-  }
+    backgroundColor: "#000033"
+  },
+	
+    
+    buttonGroup: {  
+        margin: 20,
+		flexDirection: 'row', 
+		justifyContent: 'space-between'  
+    }  
 });
+
+
