aacad4c6fc3131d3dfe6d4006019c213e2341937
dryd22 <104089664+dryd22@users.noreply.github.com>
2023-01-28 18:45:46 -0600

    Update App.js

--- App.js
+++ App.js
@@ -4,19 +4,23 @@ Button test code with returned messages to the screen user for a survey test.
 Andrew Gull using react native in expo environment.
 **/
 import React, { Component } from 'react';  
-import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';  
+import { ImageBackground, Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native';  
  
-  
-
-  
-export default class ButtonBasics extends Component {  
-    onPressButton() {  
-        //Alert.alert('You clicked the button!')  
-    }  
-  
+const image = { uri: "https://reactjs.org/logo-og.png" };  
+//attempt to add background image
+ const App = () => (
+  <View style={styles2.container}>
+    <ImageBackground source={image} resizeMode="cover" style={styles2.image}>
+      <Text style={styles2.text}>Survey</Text>
+    </ImageBackground>
+  </View>
+);
+ 
+export default class ButtonBasics extends Component {   
     render() {  
         return (  
             <View style={styles.container}>  
+	 
                 <View style={styles.buttonGroup}>  
                     <Button  
                         onPress={this.onPressButton}
@@ -54,15 +58,17 @@ export default class ButtonBasics extends Component {
   
 const styles = StyleSheet.create({  
     container: {  
-        flex: 1,
+        flex: 2,
+	    //adds white to top and bottom margins.
 		marginBottom: 20,
 		marginTop: 50,
         backgroundColor:"#000033",  
-        justifyContent: 'center',  
+        justifyContent: 'center', 
+		
     },  
+
     buttonGroup: {  
         margin:100,
-		
 		width: 150,
 		height: 150,
 		
@@ -70,9 +76,26 @@ const styles = StyleSheet.create({
     },  
     buttonGroup: {  
         margin: 50,
-		//flex:4,		
+		//flex:1,		
         flexDirection: 'row', 
 		flexWrap: 'wrap',		
         justifyContent: 'space-between'  
     }  
-})  
+})
+  const styles2 = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 3,
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
+});
