fe46e66dcbf5469dc3262bb6229ea1dfa7be44bc
Fire-fly630 <75851942+Fire-fly630@users.noreply.github.com>
2023-02-15 10:10:24 -0600

    Login page from template
    I imported the logo I created and made the colors match the color scheme we decided on

---
+++ login/src/core/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: 'blue',
+    primary: 'orange',
+    secondary: '#414757',
+    error: '#f13a59',
+  },
+}
