bbdf459ed7e9b094d0e7ce5a8f2e54f486937476
nlane510 <119709227+nlane510@users.noreply.github.com>
2023-04-24 19:45:05 -0500

    Add files via upload

--- 6cvinyl/src/screens/StartScreen.js
+++ 6cvinyl/src/screens/StartScreen.js
@@ -4,11 +4,13 @@ import Logo from '../components/Logo'
 import Header from '../components/Header'
 import Button from '../components/Button'
 import Paragraph from '../components/Paragraph'
+import LottieView from 'lottie-react-native'
 
 export default function StartScreen({ navigation }) {
   return (
     <Background>
-      <Logo />
+      <LottieView source={require('/Users/nathanlane/Desktop/Project Backup Local/Proj1/assets/lottie/88944-vinyl-loading.json')} 
+      style={{ width: '80%', aspectRatio: 1 }} autoPlay loop />
       <Header>6c Vinyl</Header>
       <Paragraph>
 Login or sign up to start your vinyl collecting journey</Paragraph>
