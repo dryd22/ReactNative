dfcbb038f095b2b7e48ed57427c6550b71c35c79
dryd22 <104089664+dryd22@users.noreply.github.com>
2023-04-25 17:57:08 -0500

    Add files via upload
    Some package updates

--- 6cvinyl/db_connection.js
+++ 6cvinyl/db_connection.js
@@ -1,16 +1,72 @@
-var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  //host: "db",
-  host: "127.0.0.1",
-  //port: "8012",
-  user: "dbadmin",
-  password: "se350",
-  database: "tester"
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 3000
+const mysql =require ('mysql');
+
+const connection = mysql.createPool ({
+host    :'localhost',
+//port  :'8012',
+user    :'admindb',
+password    :'SE350Admin',
+database    :'6CVinyl_db'
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Database is Connected!");
+
+// We are using our packages here
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+ extended: true})); 
+app.use(cors())
+
+//You can use this to check if your server is working
+//app.get('/', (req, res)=>{
+//res.send("Welcome to your server")
+//})
+app.get('/users' , function (req,res) {
+	//Connecting to DB
+	connection.getConnection (function (err, connection) {
+		//Executing query
+		connection.query('SELECT UserEmail, UserPassword FROM users', function (error, results, fields){
+			if (error) throw error;
+			res.send(results)
+		});
+	});
 });
-module.exports = connection;
+
+//Route that handles login logic
+app.post('/login', (req, res) =>{
+    console.log(req.body.UserName) 
+    console.log(req.body.UserPassword) 
+	console.log(req.body.UserName)
+})
+
+/*/Route that handles signup logic
+app.post('/users', (req, res) =>{
+	const email = req.body.email;
+const pass = req.body.pass;
+//const name = req.body.name;
+	connection.getConnection (function (err, connection) {
+		//Executing query
+		connection.query('INSERT INTO `6cvinyl_db`insert into users,  (UserEmail, UserPassword, UserName) values ( ?, ?)', 
+  [email, pass, name ], (err, result) => {
+      if(err) {
+        console.log(err)
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  )
+	});
+console.log(req.body.UserEmail) 
+console.log(req.body.UserName)
+console.log(req.body.UserPassword) 
+})*/
+
+//Start your server on a specified port
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
--- 6cvinyl/route.js
+++ 6cvinyl/route.js
@@ -1,30 +1,71 @@
-// This is a route.js file!
-const express = require('express');
-const bodyParser = require ('body-parser');
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 3000
 const mysql =require ('mysql');
 
 const connection = mysql.createPool ({
-host	:'127.0.0.1',
-//port	:'8012',
-user	:'dbadmin',
-password	:'se350',
-database	:'tester'
+host    :'34.136.183.197',
+//port  :'8012',
+user    :'admindb',
+password    :'SE350Admin',
+database    :'6CVinyl'
 });
 
-const app = express ();
-//Creating a get route that returns data from users table
-/*app.get('/users' , function (req,res) {
+
+// We are using our packages here
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+ extended: true})); 
+app.use(cors())
+
+//You can use this to check if your server is working
+//app.get('/', (req, res)=>{
+//res.send("Welcome to your server")
+//})
+app.get('/users' , function (req,res) {
 	//Connecting to DB
 	connection.getConnection (function (err, connection) {
 		//Executing query
-		connection.query('SELECT * FROM users', function (error, results, fields){
+		connection.query('SELECT UserEmail,UserPassword FROM users', function (error, results, fields){
 			if (error) throw error;
 			res.send(results)
 		});
 	});
-});*/
-
-//starting our server
-app.listen (3000, () =>{
-	console.log('Go to http://127.0.0.1:3000/users so you can see the data.');
 });
+
+//Route that handles login logic
+app.post('/login', (req, res) =>{
+    console.log(req.body.UserName) 
+    console.log(req.body.UserPassword) 
+})
+
+//Route that handles signup logic
+app.post('/users', (req, res) =>{
+	const email = req.body.email;
+const pass = req.body.pass;
+//const name = req.body.name;
+	connection.getConnection (function (err, connection) {
+		//Executing query
+		connection.query('INSERT INTO `6cvinyl_db`insert into users,  (UserEmail, UserPassword) values ( ?, ?)', 
+  [email, pass ], (err, result) => {
+      if(err) {
+        console.log(err)
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  )
+	});
+console.log(req.body.UserEmail) 
+console.log(req.body.UserName)
+console.log(req.body.UserPassword) 
+})
+
+//Start your server on a specified port
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
